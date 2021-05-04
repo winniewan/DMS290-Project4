215365e8969f6b092886926943d3f53262ea3226
emma-campbell <ecampb10@u.rochester.edu>
2021-05-04 17:32:42 -0400

    Samoyed

--- samoyed.php
+++ samoyed.php
@@ -1,125 +1,147 @@
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <title>Dogs</title>
-    <?php include "includes/head.php" ?>
-  </head>
-  
-  <body>
-    <header>
-      <?php include "includes/nav.php" ?>
-    </header>
-      <main class="container-fluid">
-        <section>
-          <h2>Samoyed</h2>
-        </section>
-        <section>
-          <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/20122208/Samoyed-standing-in-the-forest.jpg" alt="samoyed" class=""/>
-          <div>
-            <h3>History</h3>
-            <p>
-                The name Samoyed comes from the Samoyedic people (also Samodeic people), a semi-nomadic people from Asia who migrated to 
-                Siberia a thousand years ago, of the family of Sayantsi. They bred dogs for hard work in the coldest habitable places on 
-                earth. (In the Siberian town of Oymyakon, for instance, temperatures of minus-60 degrees are common.) The Samoyede people 
-                lived in tents and huddled for warmth with their dog packs during the Arctic nights. This type of human-canine cooperation 
-                for mutual survival forged a tight bond between Samoyed dogs and people. When not sledging heavy loads across vast expanses 
-                of Siberia, Samoyed dogs earned their feed as watchdogs and hunters. The Samoyede depended on reindeer as a vital source of food.
 
-                In the late 18th century, Arctic explorers returning to England introduced the Samoyed to British dog lovers. Queen Alexandra 
-                was an admirer who did much to promote the breed as a companion and show dog. In America, a sparkling white dog named Moustan 
-                of Argenteau made history in 1906 as the first of the breed (then known as the Samoyede) registered by the AKC. American and 
-                European mushers of the early 20th century knew the Samoyed as an enthusiastic hauler of supplies, capable of sledging one and 
-                a half times their own weight. Scott, Shackleton, and Amundsen are a few of the era’s famous explorers who rode behind Sammies 
-                through frozen expanses.
-            </p>
+<head>
+  <title>Samoyed</title>
+  <?php include "includes/head.php" ?>
+</head>
+
+<body>
+  <header>
+    <?php include "includes/nav.php" ?>
+  </header>
+  <main class="container-fluid">
+    <div class="card my-5 mx-5 p-4">
+      <div class="container-fluid px-4">
+
+        <!-- INTRODUCTION -->
+        <section class="text-center">
+          <h1>Samoyed</h1>
+          <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12224728/Samoyed-On-White-03.jpg" alt="samoyed" class="" />
+        </section>
+
+        <!-- HISTORY -->
+        <section>
+          <h3>History</h3>
+          <p>
+            The name Samoyed comes from the Samoyedic people (also Samodeic people), a semi-nomadic people from Asia who migrated to
+            Siberia a thousand years ago, of the family of Sayantsi. They bred dogs for hard work in the coldest habitable places on
+            earth. (In the Siberian town of Oymyakon, for instance, temperatures of minus-60 degrees are common.) The Samoyede people
+            lived in tents and huddled for warmth with their dog packs during the Arctic nights. This type of human-canine cooperation
+            for mutual survival forged a tight bond between Samoyed dogs and people. When not sledging heavy loads across vast expanses
+            of Siberia, Samoyed dogs earned their feed as watchdogs and hunters. The Samoyede depended on reindeer as a vital source of food.
+          </p>
+          <p>
+            In the late 18th century, Arctic explorers returning to England introduced the Samoyed to British dog lovers. Queen Alexandra
+            was an admirer who did much to promote the breed as a companion and show dog. In America, a sparkling white dog named Moustan
+            of Argenteau made history in 1906 as the first of the breed (then known as the Samoyede) registered by the AKC. American and
+            European mushers of the early 20th century knew the Samoyed as an enthusiastic hauler of supplies, capable of sledging one and
+            a half times their own weight. Scott, Shackleton, and Amundsen are a few of the era’s famous explorers who rode behind Sammies
+            through frozen expanses.
+          </p>
+        </section>
+
+        <!-- APPEARANCE -->
+        <section class="pt-4">
+          <h3 class="pb-2">Appearance</h3>
+          <div class="row text-light" id="attribute-row">
+            <div class="col mx-4 d-flex flex-column justify-content-center align-items-center" id="attribute-card">
+              <p>L</p>
+              <h2>Size</h2>
+            </div>
+            <div class="col mx-4 d-flex flex-column justify-content-center align-items-center" id="attribute-card">
+              <p>21-24in</p>
+              <h2>Height</h2>
+            </div>
+            <div class="col mx-4 d-flex flex-column justify-content-center align-items-center" id="attribute-card">
+              <p>35-65lb</p>
+              <h2>Weight</h2>
+            </div>
           </div>
         </section>
-        <section>
-          <div>
-            <h3>Appearance</h3>
-            <p>
-                Size: L
-            </p>
 
-            <p>
-                Height: 21-24 inches
-            </p>
-
-            <p>
-                Weight: 35-65 lbs
-            </p>
-          </div>
-        </section>
+        <!-- TEMPERAMENT -->
         <section>
-          <div>
-            <h3>Temperament</h3>
-            
-            <h4>Personality</h4>
-            <p>
-                The Samoyed is a substantial but graceful dog standing anywhere from 19 to a bit over 23 inches at the 
-                shoulder. Powerful, tireless, with a thick all-white coat impervious to cold—Sammies are perfectly beautiful 
-                but highly functional. Even their most delightful feature, a perpetual smile, has a practical function: The 
-                upturned corners of the mouth keep Sammies from drooling, preventing icicles from forming on the face. Samoyeds, 
-                the smiling sledge dogs, were bred for hard work in the world’s coldest locales. In the Siberian town of Oymyakon, 
+          <h3 class="py-4">Temperament</h3>
+          <div class="row">
+            <div class="col">
+              <h4>Personality</h4>
+              <p>
+                The Samoyed is a substantial but graceful dog standing anywhere from 19 to a bit over 23 inches at the
+                shoulder. Powerful, tireless, with a thick all-white coat impervious to cold—Sammies are perfectly beautiful
+                but highly functional. Even their most delightful feature, a perpetual smile, has a practical function: The
+                upturned corners of the mouth keep Sammies from drooling, preventing icicles from forming on the face. Samoyeds,
+                the smiling sledge dogs, were bred for hard work in the world’s coldest locales. In the Siberian town of Oymyakon,
                 for instance, temperatures of minus-60 degrees are common.
-            </p>
-            
-            <h4>Activities</h4>
-            <p>
-                Samoyeds enjoy being with their people and participating in family activities. They need some daily 
-                exercise and enjoy play sessions with their owner in a safely fenced yard or long walks on leash. The 
-                breed has a strong urge to run away and roam, and if loose a Sammie might travel for miles, putting 
-                himself at risk. These are smart, social, mischievous dogs who demand love and attention. Sammies need a 
+              </p>
+            </div>
+            <div class="col">
+              <h4>Activities</h4>
+              <p>
+                Samoyeds enjoy being with their people and participating in family activities. They need some daily
+                exercise and enjoy play sessions with their owner in a safely fenced yard or long walks on leash. The
+                breed has a strong urge to run away and roam, and if loose a Sammie might travel for miles, putting
+                himself at risk. These are smart, social, mischievous dogs who demand love and attention. Sammies need a
                 very firm but loving hand in training.
-            </p>
-
-            <h4>Training</h4>
-            <p>
-                The Samoyede people lived in tents and huddled for warmth with their dogs on brutal Arctic nights. This 
-                unusual degree of dog-and-master closeness forged a tight bond between Sammies and humankind. A Sammy 
-                sentenced to solitary confinement in the backyard is a miserable—and destructive—creature. These are smart, 
-                social, mischievous dogs who demand love and attention. Sammies need a firm but loving hand in training. 
-                Because of the depth of chest required, the legs should be moderately long. General appearance should 
+              </p>
+            </div>
+            <div class="col">
+              <h4>Training</h4>
+              <p>
+                The Samoyede people lived in tents and huddled for warmth with their dogs on brutal Arctic nights. This
+                unusual degree of dog-and-master closeness forged a tight bond between Sammies and humankind. A Sammy
+                sentenced to solitary confinement in the backyard is a miserable—and destructive—creature. These are smart,
+                social, mischievous dogs who demand love and attention. Sammies need a firm but loving hand in training.
+                Because of the depth of chest required, the legs should be moderately long. General appearance should
                 include movement and general conformation, indicating balance and good substance.
-            </p>
+              </p>
+            </div>
           </div>
         </section>
-        <section>
-          <div>
-            <h3>Care</h3>
-            
-            <h4>Health</h4>
-            <p>
-                Samoyeds are generally healthy dogs, and responsible breeders test their stock for health conditions such as hip 
-                dysplasia and eye and cardiac disorders. The Samoyed’s teeth should be brushed often, using a toothpaste designed 
-                for dogs. Regular visits to the vet for checkups and parasite control help to ensure the dog a long, healthy life.
-                The Samoyed, being essentially a working dog, should present a picture of beauty, alertness and strength, with agility, 
-                dignity and grace. As his work lies in cold climates, his coat should be heavy and weather-resistant, well groomed, 
-                and of good quality rather than quantity.
-            </p>
-            
-            <h4>Diet</h4>
-            <p>
-                The Samoyed should do well on a high-quality dog food, whether commercially manufactured or home-prepared with your 
-                veterinarian’s supervision and approval. Any diet should be appropriate to the dog’s age (puppy, adult, or senior). 
-                Some dogs are prone to getting overweight, so watch your dog’s calorie consumption and weight level. Treats can be 
-                an important aid in training, but giving too many can cause obesity. Learn about which human foods are safe for dogs, 
-                and which are not. Check with your vet if you have any concerns about your dog’s weight or diet. Clean, fresh water 
-                should be available at all times.
-            </p>
 
-            <h4>Grooming</h4>
-            <p>
-                The Samoyed has a profuse double coat, with a longer outer coat of harsh hair and a soft, thick, wooly undercoat. 
-                Samoyeds shed quite a bit all the time, even more so during shedding season, which can occur once or twice a year. 
-                Daily brushing will help to remove dirt and loose hairs and keep the dog looking his best. Mats or tangles can be 
-                worked out with a slicker brush or metal comb. The nails should be trimmed every three to four weeks or as needed. 
-                Because of the depth of chest required, the legs should be moderately long. General appearance should include 
+        <!-- CARE -->
+        <section>
+          <h3 class="pb-4">Care</h3>
+          <div class="row">
+            <div class="col">
+              <h4>Health</h4>
+              <p>
+                Samoyeds are generally healthy dogs, and responsible breeders test their stock for health conditions such as hip
+                dysplasia and eye and cardiac disorders. The Samoyed’s teeth should be brushed often, using a toothpaste designed
+                for dogs. Regular visits to the vet for checkups and parasite control help to ensure the dog a long, healthy life.
+                The Samoyed, being essentially a working dog, should present a picture of beauty, alertness and strength, with agility,
+                dignity and grace. As his work lies in cold climates, his coat should be heavy and weather-resistant, well groomed,
+                and of good quality rather than quantity.
+              </p>
+            </div>
+            <div class="col">
+              <h4>Diet</h4>
+              <p>
+                The Samoyed should do well on a high-quality dog food, whether commercially manufactured or home-prepared with your
+                veterinarian’s supervision and approval. Any diet should be appropriate to the dog’s age (puppy, adult, or senior).
+                Some dogs are prone to getting overweight, so watch your dog’s calorie consumption and weight level. Treats can be
+                an important aid in training, but giving too many can cause obesity. Learn about which human foods are safe for dogs,
+                and which are not. Check with your vet if you have any concerns about your dog’s weight or diet. Clean, fresh water
+                should be available at all times.
+              </p>
+            </div>
+            <div class="col">
+              <h4>Grooming</h4>
+              <p>
+                The Samoyed has a profuse double coat, with a longer outer coat of harsh hair and a soft, thick, wooly undercoat.
+                Samoyeds shed quite a bit all the time, even more so during shedding season, which can occur once or twice a year.
+                Daily brushing will help to remove dirt and loose hairs and keep the dog looking his best. Mats or tangles can be
+                worked out with a slicker brush or metal comb. The nails should be trimmed every three to four weeks or as needed.
+                Because of the depth of chest required, the legs should be moderately long. General appearance should include
                 movement and general conformation, indicating balance and good substance.
-            </p>
+              </p>
+            </div>
           </div>
         </section>
-      </main>
-    <?php include "includes/footer.php" ?>
-  </body>
+      </div>
+    </div>
+  </main>
+  <?php include "includes/footer.php" ?>
+</body>
+
 </html>
