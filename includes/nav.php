d5057266c1517b139ce3ff86753d8833ef598c6d
emma-campbell <ecampb10@u.rochester.edu>
2021-05-04 14:56:23 -0400

    nav styling

--- includes/nav.php
+++ includes/nav.php
@@ -1,19 +1,18 @@
-<nav class="navbar navbar-dark bg-dark">
+<nav class="navbar navbar-dark navbar-expand-md">
     <div class="container">
-        <ul class="nav navbar-nav">
-            <li class="nav-item"><a href="chihuahua.php">Chihuahua</a></li>
-            <li class="nav-item"><a href="cairn-terrier.php">Cairn Terrier</a></li>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="chihuahua.php">Chihuahua</a></li>
+            <li class="nav-item"><a class="nav-link" href="cairn-terrier.php">Cairn Terrier</a></li>
         </ul>
-        <div class="nav navbar-nav mx-auto">
-            <div class="d-flex flex-row justify-content-center align-items-center">
-                <img src="images/dog.png" alt="Logo"/>
+        <div class="nav navbar-nav" id="logo">
+            <a class="d-flex flex-row justify-content-center align-items-center" href="index.php">
+                <img src="images/dog.png" alt="Logo" />
                 <h1 class="text-light">DogSpot</h1>
-            </div>
+            </a>
         </div>
-        <ul class="nav navbar-nav">
-            <li class="nav-item"><a href="samoyed.php">Samoyed</a></li>
-            <li class="nav-item"><a href="german-shepherd.php">German Shepherd</a></li>
+        <ul class="navbar-nav">
+            <li class="nav-item"><a class="nav-link" href="samoyed.php">Samoyed</a></li>
+            <li class="nav-item"><a class="nav-link" href="german-shepherd.php">German Shepherd</a></li>
         </ul>
-
     </div>
 </nav>
