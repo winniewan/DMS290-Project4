32ff8b204d9896674ac7a052a8f4d6bc7a6475d8
emma-campbell <ecampb10@u.rochester.edu>
2021-05-04 16:01:31 -0400

    finished layout for cairn terrier

--- cairn-terrier.php
+++ cairn-terrier.php
@@ -10,114 +10,135 @@
   <header>
     <?php include "includes/nav.php" ?>
   </header>
-  <main class="container-fluid">
-    <section>
-      <h2>Cairn Terrier</h2>
-    </section>
-    <section>
-      <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12164822/Cairn-Terrier-sitting-in-the-grass.jpg" alt="cairn terrier" class="" />
-      <div>
-        <h3>History</h3>
-        <p>
-          The Cairn is one of several terrier breeds, some still extant others now extinct, who for centuries
-          patrolled the game preserves and farms of Scotland. Unravelling the histories of these breeds is difficult
-          because for so many years they were lumped together as simply Scotch terriers. Not until the late 1800s
-          did breed fanciers devise strict breeding programs and classifications for this hardy family of
-          exterminators. The name Cairn Terrier did not appear in print until 1887, though by then the Cairn-type
-          terrier had been around for a long time. Cairns were known from at least the 1600s to dwell in the Western
-          Highlands, most notably on the Isle of Skye, birthplace of their kinsman the Skye Terrier.
+  <main class="container-fluid justify-content-center align-items-center">
+    <div class="card my-5 mx-5 p-4">
+      <div class="container-fluid px-4">
 
-          Once upon a time in Scotland, a mound of stones used as a boundary or to mark a grave was called a “cairn.”
-          On Highland game preserves, rodents would live within and beneath these rock piles. The Cairn Terrier, among
-          the smallest of the go-to-ground terriers, was developed to dig into cairns and rout out the critters. When
-          grouped in packs, these plucky little hunters also worked on foxes, otters, and other predators. The Cairn’s
-          independence, courage, toughness, and alertness were qualities that served them well when digging into a cairn
-          alone and confronting sharp-toothed mammals.
-        </p>
+        <!-- INTRODUCTION -->
+        <section class="text-center">
+          <h1 class="pt-4 pb-4">Cairn Terrier</h1>
+          <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12170730/Cairn-Terrier-sitting-in-profile.jpg" alt="cairn terrier" class="img-fluid" />
+        </section> 
+        
+        <!-- HISTORY -->
+        <section>
+          <h3>History</h3>
+          <p>
+            The Cairn is one of several terrier breeds, some still extant others now extinct, who for centuries
+            patrolled the game preserves and farms of Scotland. Unravelling the histories of these breeds is difficult
+            because for so many years they were lumped together as simply Scotch terriers. Not until the late 1800s
+            did breed fanciers devise strict breeding programs and classifications for this hardy family of
+            exterminators. The name Cairn Terrier did not appear in print until 1887, though by then the Cairn-type
+            terrier had been around for a long time. Cairns were known from at least the 1600s to dwell in the Western
+            Highlands, most notably on the Isle of Skye, birthplace of their kinsman the Skye Terrier.
+          </p>
+          <p>
+            Once upon a time in Scotland, a mound of stones used as a boundary or to mark a grave was called a “cairn.”
+            On Highland game preserves, rodents would live within and beneath these rock piles. The Cairn Terrier, among
+            the smallest of the go-to-ground terriers, was developed to dig into cairns and rout out the critters. When
+            grouped in packs, these plucky little hunters also worked on foxes, otters, and other predators. The Cairn’s
+            independence, courage, toughness, and alertness were qualities that served them well when digging into a cairn
+            alone and confronting sharp-toothed mammals.
+          </p>
+        </section>
+
+        <!-- APPEARANCE -->
+        <section class="pt-4">
+          <h3 class="pb-2">Appearance</h3>
+          <div class="row text-light" id="attribute-row">
+            <div class="col mx-4 d-flex flex-column justify-content-center align-items-center" id="attribute-card">
+              <p>S</p>
+              <h2>Size</h2>
+            </div>
+            <div class="col mx-4 d-flex flex-column justify-content-center align-items-center" id="attribute-card">
+              <p>9-10in</p>
+              <h2>Height</h2>
+            </div>
+            <div class="col mx-4 d-flex flex-column justify-content-center align-items-center" id="attribute-card">
+              <p>13-14lb</p>
+              <h2>Weight</h2>
+            </div>
+          </div>
+        </section>
+
+        <!-- TEMPERAMENT -->
+        <section>
+          <h3 class="py-4">Temperament</h3>
+          <div class="row">
+            <div class="col">
+              <h4>Personality</h4>
+              <p>
+                Cairn Terriers are happy, busy little earthdogs originally bred to fearlessly root out foxes and other
+                small, furred prey in the rocky Scottish countryside. Curious and alert, Cairns like having a place
+                where they can explore and dig. The Cairn’s unique qualities, called “Cairnishness,” include a short,
+                wide head and a free-moving, short-legged body that exudes strength but not heaviness, topping out at
+                about 10 inches high and about 15 inches long. The double coat is harsh and wiry on top and downy beneath.
+                A Cairn presents as a small, shaggy, alert dog, with head, tail, and ears up, and eyes shining with intelligence.
+              </p>
+            </div>
+            <div class="col">
+              <h4>Activities</h4>
+              <p>
+                Cairns require a moderate amount of exercise and can adapt well to various living situations when
+                given daily outings. They can thrive anywhere from a Highlands farm to an urban high-rise and are
+                excellent all-around dogs. Training for dog sports provides an ideal outlet for their energy. The
+                breed exercises body and mind by participating in obedience, tracking, herding, agility, earthdog
+                events, coursing ability tests, and other activities that dog and owner can enjoy together.
+              </p>
+            </div>
+            <div class="col">
+              <h4>Training</h4>
+              <p>
+                Cairns are very smart and love their families, but they may try to test their owner’s limits, so
+                obedience training is necessary. Although they learn quickly, the Cairn may always have the instinct
+                to dig and chase small animals, so new owners should be prepared for these behaviors. Early socialization
+                and puppy training classes are recommended and help to ensure that the dog grows into a well-adjusted,
+                well-mannered companion.
+              </p>
+            </div>
+          </div>
+        </section>
+
+        <!--  CARE -->
+        <section>
+          <h3 class="pb-4">Care</h3>
+
+          <div class="row">
+            <div class="col">
+              <h4>Health</h4>
+              <p>
+                Cairn Terriers are generally healthy dogs, and responsible breeders test their stock for health disorders
+                and communicate with other dedicated breeders regularly, working together for breed health and preservation
+                of the breed’s unique qualities. These hardy little dogs can do well in hot and cold environments. Their
+                thick wiry fur and soft downy undercoat keep them warm in the winter.
+              </p>
+            </div>
+            <div class="col">
+              <h4>Diet</h4>
+              <p>
+                The Cairn Terrier should do well on a high-quality dog food, whether commercially manufactured or
+                home-prepared with your veterinarian’s supervision and approval. Any diet should be appropriate to
+                the dog’s age (puppy, adult, or senior). Some dogs are prone to getting overweight, so watch your
+                dog’s calorie consumption and weight level. Treats can be an important aid in training, but giving
+                too many can cause obesity. Learn about which human foods are safe for dogs, and which are not. Check
+                with your vet if you have any concerns about your dog’s weight or diet. Clean, fresh water should be
+                available at all times.
+              </p>
+            </div>
+            <div class="col">
+              <h4>Grooming</h4>
+              <p>
+                Cairn Terriers are generally easy keepers, although weekly brushing and combing are recommended, as
+                well as periodic hand-stripping to maintain the coat’s texture. Having a comb and soft slicker brush
+                handy will help with the grooming tasks. With a new puppy, spending time together in grooming sessions
+                helps to accustom him or her to being worked with and is an opportunity to develop the bond between you.
+                The nails should be trimmed regularly, as overly long nails can cause the dog discomfort.
+              </p>
+            </div>
+          </div>
+        </section>
       </div>
-    </section>
-    <section>
-      <div>
-        <h3>Appearance</h3>
-        <p>
-          Size: S
-        </p>
-
-        <p>
-          Height: 9-10 inches
-        </p>
-
-        <p>
-          Weight: 13-14 lbs
-        </p>
-      </div>
-    </section>
-    <section>
-      <div>
-        <h3>Temperament</h3>
-
-        <h4>Personality</h4>
-        <p>
-          Cairn Terriers are happy, busy little earthdogs originally bred to fearlessly root out foxes and other
-          small, furred prey in the rocky Scottish countryside. Curious and alert, Cairns like having a place
-          where they can explore and dig. The Cairn’s unique qualities, called “Cairnishness,” include a short,
-          wide head and a free-moving, short-legged body that exudes strength but not heaviness, topping out at
-          about 10 inches high and about 15 inches long. The double coat is harsh and wiry on top and downy beneath.
-          A Cairn presents as a small, shaggy, alert dog, with head, tail, and ears up, and eyes shining with intelligence.
-        </p>
-
-        <h4>Activities</h4>
-        <p>
-          Cairns require a moderate amount of exercise and can adapt well to various living situations when
-          given daily outings. They can thrive anywhere from a Highlands farm to an urban high-rise and are
-          excellent all-around dogs. Training for dog sports provides an ideal outlet for their energy. The
-          breed exercises body and mind by participating in obedience, tracking, herding, agility, earthdog
-          events, coursing ability tests, and other activities that dog and owner can enjoy together.
-        </p>
-
-        <h4>Training</h4>
-        <p>
-          Cairns are very smart and love their families, but they may try to test their owner’s limits, so
-          obedience training is necessary. Although they learn quickly, the Cairn may always have the instinct
-          to dig and chase small animals, so new owners should be prepared for these behaviors. Early socialization
-          and puppy training classes are recommended and help to ensure that the dog grows into a well-adjusted,
-          well-mannered companion.
-        </p>
-      </div>
-    </section>
-    <section>
-      <div>
-        <h3>Care</h3>
-
-        <h4>Health</h4>
-        <p>
-          Cairn Terriers are generally healthy dogs, and responsible breeders test their stock for health disorders
-          and communicate with other dedicated breeders regularly, working together for breed health and preservation
-          of the breed’s unique qualities. These hardy little dogs can do well in hot and cold environments. Their
-          thick wiry fur and soft downy undercoat keep them warm in the winter.
-        </p>
-
-        <h4>Diet</h4>
-        <p>
-          The Cairn Terrier should do well on a high-quality dog food, whether commercially manufactured or
-          home-prepared with your veterinarian’s supervision and approval. Any diet should be appropriate to
-          the dog’s age (puppy, adult, or senior). Some dogs are prone to getting overweight, so watch your
-          dog’s calorie consumption and weight level. Treats can be an important aid in training, but giving
-          too many can cause obesity. Learn about which human foods are safe for dogs, and which are not. Check
-          with your vet if you have any concerns about your dog’s weight or diet. Clean, fresh water should be
-          available at all times.
-        </p>
-
-        <h4>Grooming</h4>
-        <p>
-          Cairn Terriers are generally easy keepers, although weekly brushing and combing are recommended, as
-          well as periodic hand-stripping to maintain the coat’s texture. Having a comb and soft slicker brush
-          handy will help with the grooming tasks. With a new puppy, spending time together in grooming sessions
-          helps to accustom him or her to being worked with and is an opportunity to develop the bond between you.
-          The nails should be trimmed regularly, as overly long nails can cause the dog discomfort.
-        </p>
-      </div>
-    </section>
+    </div>
   </main>
   <?php include "includes/footer.php" ?>
 </body>
