ab9958fea4c8399dcde609e0ff10584112b2e2e0
emma-campbell <ecampb10@u.rochester.edu>
2021-05-04 14:38:53 -0400

    centered log + installed fonts

--- includes/nav.php
+++ includes/nav.php
@@ -1,11 +1,19 @@
-<nav>
-    <h1><a href="index.html">Dogs</a></h1>
-    <div class="menu">
-        <ul>
-            <li><a href="chihuahua.php">Chihuahua</a></li>
-            <li><a href="cairn-terrier.php">Cairn Terrier</a></li>
-            <li><a href="samoyed.php">Samoyed</a></li>
-            <li><a href="german-shepherd.php">German Shepherd</a></li>
+<nav class="navbar navbar-dark bg-dark">
+    <div class="container">
+        <ul class="nav navbar-nav">
+            <li class="nav-item"><a href="chihuahua.php">Chihuahua</a></li>
+            <li class="nav-item"><a href="cairn-terrier.php">Cairn Terrier</a></li>
         </ul>
+        <div class="nav navbar-nav mx-auto">
+            <div class="d-flex flex-row justify-content-center align-items-center">
+                <img src="images/dog.png" alt="Logo"/>
+                <h1 class="text-light">DogSpot</h1>
+            </div>
+        </div>
+        <ul class="nav navbar-nav">
+            <li class="nav-item"><a href="samoyed.php">Samoyed</a></li>
+            <li class="nav-item"><a href="german-shepherd.php">German Shepherd</a></li>
+        </ul>
+
     </div>
 </nav>
