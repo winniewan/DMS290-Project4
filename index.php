05def29308ea36b17d384402efdac510e2c0513e
Winnie Wan <winniewan6@gmail.com>
2021-05-05 17:26:51 -0400

    add links to home page subtitles

--- index.php
+++ index.php
@@ -16,21 +16,21 @@
       <div class="row justify-content-center">
         <div class="col-sm-4">
           <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/27134650/Chihuahua-standing-in-three-quarter-view.jpg" alt="chiuahua" class="img-fluid" />
-          <h3>Chihuahua</h3>
+          <h3><a class="nav-link" href="chihuahua.php">Chihuahua</a></h3>
         </div>
         <div class="col-sm-4">
           <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12164822/Cairn-Terrier-sitting-in-the-grass.jpg" alt="cairn terrier" class="img-fluid" />
-          <h3>Cairn Terrier</h3>
+          <h3><a class="nav-link" href="cairn-terrier.php">Cairn Terrier</a></h3>
         </div>
       </div>
       <div class="row justify-content-center">
         <div class="col-sm-4">
           <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/20122208/Samoyed-standing-in-the-forest.jpg" alt="samoyed" class="img-fluid" />
-          <h3>Samoyed</h3>
+          <h3><a class="nav-link" href="samoyed.php">Samoyed</a></h3>
         </div>
         <div class="col-sm-4">
           <img src="https://s3.amazonaws.com/cdn-origin-etr.akc.org/wp-content/uploads/2017/11/12213218/German-Shepherd-on-White-00.jpg" alt="german shepherd" class="img-fluid" />
-          <h3>German Shepherd</h3>
+          <h3><a class="nav-link" href="german-shepherd.php">German Shepherd</a></h3>
         </div>
       </div>
     </section>
